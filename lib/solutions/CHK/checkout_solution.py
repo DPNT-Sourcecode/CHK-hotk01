1364768b65fb1e68787ce00e09a38971a98f75c1
sbx_user1059 <sbx_user1059@ip-10-129-213-211.eu-west-2.compute.internal>
2018-10-27 18:19:08 +0000

    Sat Oct 27 18:19:08 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,10 +5,35 @@
 def checkout(skus):
     #raise NotImplementedError()
     import math
-    from collections import counter
+    from collections import Counter
+    # Create list , assuming input string  will be having A,A,B,C,D,E format
     lis=[]
     lis=skus.split(",")
+    totalVal=0
     if not lis:
         return -1
-    else
+    else:
+        dir_sku=Counter(lis)
+        for item, val in dir_sku.items():
+            if item =='A':
+                if val <3:
+                    A_sum=val*50
+                else:
+                    reminder=val%3
+                    indVal=int(math.floor(val/3))
+                    A_sum=(indVal*130)+(reminder*50)
+            elif item =='B':
+                 if val < 2:
+                     B_sum = val * 30
+                 else:
+                     reminder = val % 2
+                     indVal = int(math.floor(val / 2))
+                     B_sum = (indVal * 45) + (reminder * 30)
 
+        totalVal=A_sum+B_sum
+        return totalVal
+
+
+sku =('B,B,B,C,D')
+ret=checkout(sku)
+print(ret)
