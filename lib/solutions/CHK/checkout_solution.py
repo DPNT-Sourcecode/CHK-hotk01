205131cfa43f2eba1f0ab0e3ed1f37cc0a6e76eb
sbx_user1059 <sbx_user1059@ip-10-129-213-211.eu-west-2.compute.internal>
2018-10-28 00:20:06 +0000

    Sun Oct 28 00:20:06 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -26,50 +26,49 @@ def module1(val,sku1,price1,sku2,price2,baseprice):
 
     return retVal
 
-def moodule2(val,baseCnt,basePrice,origVal,origBasePrice,OrigOfferPrice,OrigOfferCnt):
+def moodule2(val,baseCnt,basePrice,origVal,origBasePrice,OrigOfferPrice,OrigOfferCnt): #2f 1 b
     import math
     retVal=0
-    if item == 'E':
-        BtotalVal = 0
-        if val < 2:
-            print(1)
-            totalVal = (val * 40) + totalVal
-            # totalVal = (b_val * 30) + totalVal
-            if (b_val < 2 and b_val > 0):
-                totalVal = (b_val * 30) + totalVal
-            elif (b_val > 0):
-                reminder = b_val % 2
-                indVal = int(math.floor(b_val / 2))
-                totalVal = ((indVal * 45) + (reminder * 30)) + totalVal
-            elif (b_val == 2):
-                totalVal = 45 + totalVal
-        elif val == 2:
-            print(2)
-            totalVal = (val * 40) + totalVal
-            # print (totalVal)
-            b_new_val = (b_val - 1)
-            if (b_new_val < 2 and b_new_val > 0):
-                # print (b_new_val)
-                totalVal = (b_new_val * 30) + totalVal
-            elif (b_new_val > 2):
-                reminder = b_new_val % 2
-                indVal = int(math.floor(b_new_val / 2))
-                totalVal = ((indVal * 45) + (reminder * 30)) + totalVal
-            elif (b_new_val == 2):
-                totalVal = 45 + totalVal
-        elif val > 2:
-            # print(3)
-            totalVal = (val * 40) + totalVal
-            indVal = int(math.floor(val / 2))
-            b_new_val = (b_val - indVal)
-            if (b_new_val < 2 and b_new_val > 0):
-                totalVal = (b_new_val * 30) + totalVal
-            elif (b_new_val > 2):
-                reminder = b_new_val % 2
-                indVal = int(math.floor(b_new_val / 2))
-                totalVal = ((indVal * 45) + (reminder * 30)) + totalVal
-            elif (b_new_val == 2):
-                totalVal = 45 + totalVal
+    #BtotalVal = 0
+    if val < baseCnt:
+        print(1)
+        retVal = (val * basePrice) + totalVal
+        # totalVal = (b_val * 30) + totalVal
+        if (b_val < 2 and b_val > 0):
+            totalVal = (b_val * 30) + totalVal
+        elif (b_val > 0):
+            reminder = b_val % 2
+            indVal = int(math.floor(b_val / 2))
+            totalVal = ((indVal * 45) + (reminder * 30)) + totalVal
+        elif (b_val == 2):
+            totalVal = 45 + totalVal
+    elif val == 2:
+        print(2)
+        totalVal = (val * 40) + totalVal
+        # print (totalVal)
+        b_new_val = (b_val - 1)
+        if (b_new_val < 2 and b_new_val > 0):
+            # print (b_new_val)
+            totalVal = (b_new_val * 30) + totalVal
+        elif (b_new_val > 2):
+            reminder = b_new_val % 2
+            indVal = int(math.floor(b_new_val / 2))
+            totalVal = ((indVal * 45) + (reminder * 30)) + totalVal
+        elif (b_new_val == 2):
+            totalVal = 45 + totalVal
+    elif val > 2:
+        # print(3)
+        retVal = (val * basePrice)
+        indVal = int(math.floor(val / 2))
+        b_new_val = (b_val - indVal)
+        if (b_new_val < 2 and b_new_val > 0):
+            totalVal = (b_new_val * 30) + totalVal
+        elif (b_new_val > 2):
+            reminder = b_new_val % 2
+            indVal = int(math.floor(b_new_val / 2))
+            totalVal = ((indVal * 45) + (reminder * 30)) + totalVal
+        elif (b_new_val == 2):
+            totalVal = 45 + totalVal
 
 
 
