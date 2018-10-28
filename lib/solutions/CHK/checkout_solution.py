128e6e0f98804a8a1341e31933dc89fdde3f8edd
sbx_user1060 <sbx_user1060@ip-10-129-114-128.eu-west-2.compute.internal>
2018-10-28 17:32:10 +0000

    Sun Oct 28 17:32:10 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -95,6 +95,9 @@ def module5(qty,val,price,basePrice):
 
 def module6(z_val,z_price,x_val, x_price,s_val,s_price):
     totalVal= z_val+x_val+s_val
+    ZtotalVal=0
+    StotalVal=0
+    retVal=0
     if totalVal>3:
         reminder=totalVal%3
         indVal=totalVal/3
@@ -102,7 +105,9 @@ def module6(z_val,z_price,x_val, x_price,s_val,s_price):
             retVal=indVal*45
         elif z_val>0 and s_val>0 and x_val==0:
             if z_val>s_val:
-                
+                ZtotalVal=module5(3, z_val, 45, 21)
+                StotalVal = module5(3, z_val, 45, 20)
+                retVal=ZtotalVal+StotalVal
 
     return retVal
 
