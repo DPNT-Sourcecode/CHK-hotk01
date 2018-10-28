438904f7a3044bd05300819eeec51a4f1b60c9a1
sbx_user1060 <sbx_user1060@ip-10-129-114-128.eu-west-2.compute.internal>
2018-10-28 20:13:53 +0000

    Sun Oct 28 20:13:53 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -199,6 +199,13 @@ def module7(z_val,z_price,x_val, x_price,s_val,s_price):
             if (cntr+1)%3==0 and cntr!=0:
              val= val+45
              print('finalVal',finalVal)
+             print('cntr', cntr)
+            elif lis[cntr+1]=='s' and cntr+1>3:
+                val = val + s_price
+            elif lis[cntr+1]=='x' and cntr+1>3:
+                val = val + x_price
+            elif lis[cntr+1]=='z' and cntr+1>3:
+                val = val + z_price
             cntr = cntr + 1
         else:
             if finalVal=='s':
